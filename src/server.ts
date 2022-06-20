c95cc3b370e8c4f6f52dd19ab3578c740f448fb3
erikefernandes40 <erikefernandes40@gmail.com>
2022-06-20 11:46:48 -0300

    feat: adição da documentação com swagger

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express from 'express'
+import swaggerUi from "swagger-ui-express"
+import swaggerFile from "./swagger.json"
+
 import { router } from './routes'
 const app = express()
 
 app.use(express.json())
 
-app.use(router)
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 app.get("/", (request, response) => {
   response.json({mesage: "hello world!!"})
 })
 
+app.use(router)
 
 app.listen(4000, () => console.log("Server is running!"))
