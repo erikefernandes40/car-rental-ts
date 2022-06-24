8fba1e05e1bcfa2762d9b91fd5bc74a81d436b2b
erikefernandes40 <erikefernandes40@gmail.com>
2022-06-24 11:21:03 -0300

    feat: adiçao e arquivos de configuração do prisma

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import swaggerUi from "swagger-ui-express"
 import swaggerFile from "./swagger.json"
 import { router } from './routes'
+
 const app = express()
 
 app.use(express.json())
